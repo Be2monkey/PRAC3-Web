3a73a5b291507348202b2503190e56f613d09342
Dinakeri <117737175+Dinakeri@users.noreply.github.com>
2024-11-25 09:40:37 +0100

    logo netjes gemaakt
    boven de lijn gezet zodat het er beter uitziet

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <img src="img/schoolvoetbal.png" alt="schoolvoetbal" width="110" height="200">
+    <img class="block mb-5" src="img/schoolvoetbal.png" alt="schoolvoetbal" width="110" height="200">
 </svg>
