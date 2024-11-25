df3acbbc677ee5d71c1004fe576de2ec62ec8f6b
Be2monkey <D303894@edu.curio.nl>
2024-11-25 10:51:43 +0100

    create werkend gekregen

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -9,7 +9,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{ __("Teams overzicht:") }}
+
+                    <div>
+                        <form action="{{route('teams.store')}}" method="POST">
+                            @csrf
+                            <label>Team name</label>
+                            <input type="string" name="teamName"><br>
+
+                            <label>Number of players</label>
+                            <input type="number" name="numberOfPlayers"><br>
+
+                            <label>player names</label>
+                            <textarea rows="10" name="playerNames"></textarea><br>
+                            <input type="submit">
+                        </form>
+                    </div>
 
                 </div>
             </div>
