3e7f189ffe7b2b5c82568aa11be764beda88a3b6
Dinakeri <117737175+Dinakeri@users.noreply.github.com>
2024-11-25 10:58:51 +0100

    css verandering
     voor gezorgd dat tekst op zwart blijft

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -11,18 +11,18 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("Teams overzicht:") }}
 
-                    <div>
+                    <div >
                         <form action="{{route('teams.store')}}" method="POST">
                             @csrf
                             <label>Team name</label>
-                            <input type="string" name="teamName"><br>
+                            <input class="text-black" type="string" name="teamName"><br>
 
                             <label>Number of players</label>
-                            <input type="number" name="numberOfPlayers"><br>
+                            <input class="text-black" type="number" name="numberOfPlayers"><br>
 
                             <label>player names</label>
-                            <textarea rows="10" name="playerNames"></textarea><br>
-                            <input type="submit">
+                            <textarea class="text-black" rows="10" name="playerNames"></textarea><br>
+                            <input  type="submit">
                         </form>
                     </div>
 
