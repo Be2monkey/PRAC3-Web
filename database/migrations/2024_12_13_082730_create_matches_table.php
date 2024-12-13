268f3e8a90f8c5d31d6c754719d546a6706978bb
Femkedelaat <noth4wkss@outlook.com>
2024-12-13 09:59:03 +0100

    matches migration

---
+++ database/migrations/2024_12_13_082730_create_matches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->uint('team1_id');
+            $table->uint('team2_id');
+            $table->integer('team1_score')->nullable();
+            $table->int('team2_score')->nullable();
+            $table->text('field');
+            $table->uint('referee_id');
+            $table->string('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matches');
+    }
+};
